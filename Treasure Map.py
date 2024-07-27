5e25f48a9a986d9f629f08d5e63320a822c2d39a
Latika <139051585+latika6@users.noreply.github.com>
2024-07-28 00:26:55 +0530

    Update Treasure Map.py

--- Treasure Map.py
+++ Treasure Map.py
@@ -22,7 +22,7 @@ position = input() # Where do you want to put the treasure?
 letter = position[0].lower()
 abc = ['a', 'b', 'c']
 letter_index = abc.index(letter)
-number_index = int(position[1]-1)
+number_index = int(position[1])-1)
 map[number_index][letter_index] = "X"
 # Write your code above this row 👆
 # 🚨 Don't change the code below 👇
