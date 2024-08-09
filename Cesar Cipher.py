9ccf3825a7893d84bed847091d6e4939ebdd37a5
Latika <139051585+latika6@users.noreply.github.com>
2024-08-09 23:49:49 +0530

    Update Cesar Cipher.py

--- Cesar Cipher.py
+++ Cesar Cipher.py
@@ -43,8 +43,6 @@
 #     print(cipher_text_second)
 # ceasar(original_text = text, shift_amount = shift, encode_or_decode =direction)
 
-
-# Define the Caesar cipher function
 def ceasar(original_text, shift_amount, encode_or_decode):
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     cipher_text_second = ""
@@ -59,18 +57,14 @@ def ceasar(original_text, shift_amount, encode_or_decode):
             cipher_text_second += alphabet[shifted_decrypt_position]
     print(cipher_text_second)
 
-# Main loop to continue or stop based on user input
 should_continue = True
 while should_continue:
-    # Take inputs from the user
     direction = input("Type 'encode' to encrypt, 'decode' to decrypt:\n").lower()
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
     
-    # Call the Caesar cipher function
     ceasar(original_text=text, shift_amount=shift, encode_or_decode=direction)
     
-    # Ask the user if they want to restart or exit
     restart = input("Do you want to go again? Type 'yes' or 'no':\n").lower()
     
     if restart == "no":
