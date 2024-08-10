035ce9968d18d116950c190165f62a03095b4b6c
Latika <139051585+latika6@users.noreply.github.com>
2024-08-10 23:41:42 +0530

    Add files via upload

---
+++ Random.py
@@ -0,0 +1,10 @@
+# Write a code to generate 10 random integers between 20 to 40 (Is it possible to generate same random numbers everytime? If yes, describe the function)
+# type your code here
+import random
+random_number = np.random.randint(20,41,10)
+random_number
+
+# if we try to fix the random numbers, we can use seed function
+random_number = np.random.seed(seed=12)
+random_number = np.random.randint(20,41,10)
+random_number
