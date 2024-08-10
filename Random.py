83b53466ccdb9cf769a0d18c245bf67893b7425b
Latika <139051585+latika6@users.noreply.github.com>
2024-08-10 23:42:36 +0530

    Update Random.py

--- Random.py
+++ Random.py
@@ -1,5 +1,6 @@
 # Write a code to generate 10 random integers between 20 to 40 (Is it possible to generate same random numbers everytime? If yes, describe the function)
 # type your code here
+import numpy as np
 import random
 random_number = np.random.randint(20,41,10)
 random_number
