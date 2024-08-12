cde1b35cab3b7ec56903e4093b1a3870a8e576f1
Latika <139051585+latika6@users.noreply.github.com>
2024-08-12 21:20:31 +0530

    Add files via upload

---
+++ iloc.py
@@ -0,0 +1,10 @@
+# type your code here
+words_list = ['words', 'will', 'where', 'shall', 'we', 'will', 'shall', 'we', 'words', 'where', 'shall',
+            'will', 'there', 'wow', 'should', 'shall', 'we', 'where', 'should', 'where', 'will', 
+            'there', 'now', 'where', 'we', 'will', 'where', 'should', 'will', 'where']
+
+words_data_frame = pd.DataFrame(words_list)
+words_data_frame
+
+counts_words = words_data_frame.value_counts()
+counts_words.iloc[0:5]
