f6c944c9fcd0ed2a66d95533d1b7e62455312fac
Latika <139051585+latika6@users.noreply.github.com>
2024-08-12 13:28:00 +0530

    Update Split(name).py

--- Split(name).py
+++ Split(name).py
@@ -1,4 +1,6 @@
 # type your code here
+import numpy as np
+import pandas as pd
 name = ['Emma Larter', 'Mia Junior', 'Sophia Depp', 'James Smith']
 salary = [3200, 4500, 3600, 5596]
 df = pd.DataFrame({'Name': name, 'Salary': salary})
