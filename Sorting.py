a4af08a43afe247a5086c0ae0f0db6beb923ef92
Latika <139051585+latika6@users.noreply.github.com>
2024-08-12 21:34:04 +0530

    Update Sorting.py

--- Sorting.py
+++ Sorting.py
@@ -9,3 +9,18 @@ df_store = pd.DataFrame(
 )
 df_store
 df_store.sort_values(by = 'Sales', ascending=False)
+
+# or 
+
+# type your code here
+df_store = pd.DataFrame(
+{
+'Store': ['A', 'B', 'A', 'C', 'D', 'A', 'D', 'A', 'C', 'B'],
+'Location': ['Mumbai', 'Pune', 'Hyderabad', 'Mumbai', 'Pune', 'Delhi', 'Hyderabad', 'Pune', 'Mumbai', 'Pune'],
+'Sales': [40000, 45000, 56000, 90000, 89000, 87000, 85000, 78000, 89000, 70000]
+}
+)
+df_store
+# for two columns
+df_store.sort_values(by = ['Sales','Store'], ascending=[False, True])
+df_store
