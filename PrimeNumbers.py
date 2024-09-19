d2c3d0405871c23c444547fb4865eab09dba039b
Latika <139051585+latika6@users.noreply.github.com>
2024-09-19 16:55:21 +0530

    Create PrimeNumbers.py

---
+++ PrimeNumbers.py
@@ -0,0 +1,8 @@
+#  Print All Prime Numbers in an Interval of 1–10
+for i in range(1, 11):
+    if i > 1:
+        for j in range(2, i):
+            if (i%j) == 0:
+                break
+        else:
+            print(i)
