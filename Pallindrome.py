08fafdf998c8587f5a713449fd57418821c9ad71
Latika <139051585+latika6@users.noreply.github.com>
2024-09-20 11:56:43 +0530

    Create Pallindrome.py

---
+++ Pallindrome.py
@@ -0,0 +1,8 @@
+# Check if a String is Palindrome
+def is_pallindrome(s):
+    s = s[::-1]
+s = "madam"
+if is_pallindrome(s):
+    print("It is pallindrome")
+else:
+    print("It is not pallindrome")
