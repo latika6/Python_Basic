702f3c5dfd40a3b54b55adcaa4672b651bfea4ec
Latika <139051585+latika6@users.noreply.github.com>
2025-04-01 23:22:49 +0530

    Create FastAPI.py

---
+++ FastAPI.py
@@ -0,0 +1,16 @@
+# pip install fastapi uvicorn
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, FastAPI!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "query": q}
+
+# uvicorn main:app --reload
+# http://127.0.0.1:8000/
