f1744ceb251ae9023d9fe2ac74387d08ce3e5a0c
Latika <139051585+latika6@users.noreply.github.com>
2025-04-01 23:58:20 +0530

    Update VowelsCount.py

--- VowelsCount.py
+++ VowelsCount.py
@@ -8,3 +8,15 @@ def count_vowels(s):
     return count
 s = "hello"
 print(f"Number of vowels in the string: {count_vowels(s)}")
+
+or
+
+# Count Vowels in a String
+vowels = "aeiou"
+string = "Latika"
+string.lower()
+count = 0
+for char in string:
+    if char in vowels:
+        count +=1
+print(count)
